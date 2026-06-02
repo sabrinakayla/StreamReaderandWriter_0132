10fc73897eb07ed97903fd1c5fd431b3a0c8b0c5
sabrinakayla <sabrinakuliah10@gmail.com>
2026-06-02 17:58:16 +0700

    inisialisasi file stream

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -11,3 +11,9 @@ int main()
     //membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+    //membuat file dalam mode menulis
+    ofstream outfile;
+    //menunjuk ke sebuah nama file dengan ekstentsi txt
+    outfile.open(NamaFile + ".txt", ios::out);
+
+    cout << ">= Menulis file, '\\q\\' untuk keluar" << endl;
